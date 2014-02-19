70a7e8e01fa0ab2f77599b26391732437905b418
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-02-18 23:51:33 -0500

    Base project setup done

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject chainreact-clj "0.0.1"
+        :description "Clojure version of the Chain Reaction android game"
+        :dependencies [[org.clojure/clojure "1.5.1"] [org.clojure/data.json "0.2.4"]]
+        :main chainreact-clj.core)
+
---
+++ src/chainreact_clj/core.clj
@@ -0,0 +1,7 @@
+(ns chainreact-clj.core
+  (:gen-class)
+  (:require [clojure.data.json :as json])
+)
+
+(defn -main [& args]
+  (println "Hello Chain React"))
