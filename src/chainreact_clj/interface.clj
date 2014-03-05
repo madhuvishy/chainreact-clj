d3a4fa086658f715bb011c61d21f7548e7ffba24
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-03-05 13:45:40 -0500

    Pretty printed game works with players flipping

--- src/chainreact_clj/interface.clj
+++ src/chainreact_clj/interface.clj
@@ -7,13 +7,37 @@
   [input]
   (map #(read-string %) (split input #" ")))
 
+(defn get-print-value
+  [cell]
+  (let [owner (:owner cell)
+        curr (:curr-orbs cell)
+        maxm (:max-orbs cell)]
+    (str 
+      (apply str (repeat curr owner)) 
+      (apply str (repeat (- maxm curr) "_")))))
+
+
+(defn get-row-values
+  [row board board-size]
+  (for [col (range board-size)] 
+    (let [cell (get-in board [row col])] 
+      (if (= cell nil) 
+        "____" 
+        (get-print-value cell)))))
+
+
 (defn draw-board 
   [board board-size]
-  ;(pprint (for [x (range board-size)] 
-  ;            (repeat board-size "++++") 
-  ;          ))
-  (println board)
+  (pprint (for [row (range board-size)]
+            (get-row-values row board board-size)))
   )
+
+(defn get-next-player
+  [old-player player-size]
+  (if (= player-size (inc old-player))
+        0
+        (inc old-player)))   
+
 
 (defn -main [& args]
   (let [board-size (read-string (first args))
@@ -28,6 +52,9 @@
                               (first (split-input input))
                               (second (split-input input))
                               player
-                              board-size)]
+                              board-size)
+                  new-player (if (= new-board board) 
+                               player
+                               (get-next-player player player-size))]
               (draw-board new-board board-size) 
-              (recur (read-line) new-board player))))))
+              (recur (read-line) new-board new-player ))))))
