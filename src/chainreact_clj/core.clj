8ca748246c1ea28ad18df4b5b15bd7e830c13b60
Zachary Allaun <zach.allaun@gmail.com>
2014-03-10 14:00:50 -0400

    Let my editor indent everything for me

--- src/chainreact_clj/core.clj
+++ src/chainreact_clj/core.clj
@@ -1,163 +1,162 @@
 (ns chainreact-clj.core)
 
-;Defines a map of the possible orb counts for a type of cell
+;;Defines a map of the possible orb counts for a type of cell
 (def orb-counts {:corner 2 :edge 3 :all 4})
 
-;Defines a map of the 4 possible move combinations for every row, col pair
+;;Defines a map of the 4 possible move combinations for every row, col pair
 (def moves '({:x -1 :y 0} {:x 1 :y 0} {:x 0 :y -1} {:x 0 :y 1}))
 
 (declare get-new-board)
 
-;Checks if a row and column are valid and returns a Boolean result
+;;Checks if a row and column are valid and returns a Boolean result
 (defn is-valid-rc
   [row col board-size]
   (and (>= row 0) (< row board-size) (>= col 0) (< col board-size)))
 
-;Given row and column, return how many maximum orbs a cell can have
-;Corners have 2, Edges 3 and Rest 4
+;;Given row and column, return how many maximum orbs a cell can have
+;;Corners have 2, Edges 3 and Rest 4
 (defn get-max-orbs
   [row col board-size]
   (cond
-    ;Check if valid row and column, if not return nil
-    (not (is-valid-rc row col board-size))
-      nil
-    ;If row and col are (0,0) (0,boardsize-1) (boardsize-1,0) (boardsize-1,boardsize-1)
-    ;It's a corner so return 2   
-    (and (= (mod row (- board-size 1))  0)  (= (mod col (- board-size 1)) 0) )
-      (:corner orb-counts)
-    ;One of row or column being 0 or boardsize-1 and 
-    ;other not being one of these two decides edge
-    (or 
-      (and (= (mod row (- board-size 1))  0)  (not= (mod col (- board-size 1)) 0) )
-      (and (not= (mod row (- board-size 1))  0)  (= (mod col (- board-size 1)) 0) ))
-      (:edge orb-counts)
-    ;Anything else returns 4
-    :else
-      (:all orb-counts)
-    ))
+   ;;Check if valid row and column, if not return nil
+   (not (is-valid-rc row col board-size))
+   nil
+   ;;If row and col are (0,0) (0,boardsize-1) (boardsize-1,0) (boardsize-1,boardsize-1)
+   ;;It's a corner so return 2
+   (and (= (mod row (- board-size 1))  0)  (= (mod col (- board-size 1)) 0) )
+   (:corner orb-counts)
+   ;;One of row or column being 0 or boardsize-1 and
+   ;;other not being one of these two decides edge
+   (or
+    (and (= (mod row (- board-size 1))  0)  (not= (mod col (- board-size 1)) 0) )
+    (and (not= (mod row (- board-size 1))  0)  (= (mod col (- board-size 1)) 0) ))
+   (:edge orb-counts)
+   ;;Anything else returns 4
+   :else
+   (:all orb-counts)
+   ))
 
-;Creates a replicated list of row col maps
+;;Creates a replicated list of row col maps
 (defn rc-values [row col]
-  (replicate 4 {:x row :y col})) 
+  (replicate 4 {:x row :y col}))
 
-;Sums each move combination with the row col pair and returns all moves
+;;Sums each move combination with the row col pair and returns all moves
 (defn all-moves
   [row col]
-  (map 
-    (fn [move rc]  
-      {:x (+ (:x move)(:x rc)) 
-       :y (+ (:y move) (:y rc))}) 
-    moves 
-    (rc-values row col)))
+  (map
+   (fn [move rc]
+     {:x (+ (:x move)(:x rc))
+      :y (+ (:y move) (:y rc))})
+   moves
+   (rc-values row col)))
 
-;Filters the result of all-moves and returns only valid ones
+;;Filters the result of all-moves and returns only valid ones
 (defn valid-moves
   [row col board-size]
-  (filter 
-    (fn [move] 
-      (and 
-        (>= (:x move) 0) (>= (:y move) 0) 
-        (< (:x move) board-size) (< (:y move) board-size))) 
-    (all-moves row col)))
+  (filter
+   (fn [move]
+     (and
+      (>= (:x move) 0) (>= (:y move) 0)
+      (< (:x move) board-size) (< (:y move) board-size)))
+   (all-moves row col)))
 
 
-;THE CHAIN REACTION- The Magic Happens Here
-;For every possible valid move around the given cell, increment curr orbs, set owner to player and recurse
+;; THE CHAIN REACTION- The Magic Happens Here
+;; For every possible valid move around the given cell, increment curr orbs, set owner to player and recurse
 
 (defn chain-react
   [old-board row col player board-size]
-    (let [moves-list (valid-moves row col board-size)]
-      (loop [moves moves-list index 0 board old-board]
-        (if (>= index (count moves-list)) 
-          board
-          (when (seq moves)
-            (let [x (:x (first moves))
+  (let [moves-list (valid-moves row col board-size)]
+    (loop [moves moves-list index 0 board old-board]
+      (if (>= index (count moves-list))
+        board
+        (when (seq moves)
+          (let [x (:x (first moves))
                 y (:y (first moves))]
             (recur (rest moves) (inc index) (get-new-board board x y player board-size true))))
         ))))
 
 
-;Given a row and column and the player returns an altered version of the board
-;Cases:
-;IF Valid Row and Column, then:
-; 1. If the row doesn't exist, assoc row and column, set player, max orbs, set current orbs as 1 and return
-; 2. If row exists but column doesnt, add column, set player, max orbs, set current orbs as 1 and return
-; 3. If the row and column exist, one of these things happen
-;    3a. :curr-orbs+1 is < :max-orbs
-;         3a1. :owner and player don't match -> Do nothing 
-;         3a2. :owner and player match -> Increment current orbs and send back a new map
-;    3b. :curr-orbs+1 is >= :max-orbs
-;         3b1. Chain react, all orbs in the path now have :owner = player
+;; Given a row and column and the player returns an altered version of the board
+;; Cases:
+;; IF Valid Row and Column, then:
+;; 1. If the row doesn't exist, assoc row and column, set player, max orbs, set current orbs as 1 and return
+;; 2. If row exists but column doesnt, add column, set player, max orbs, set current orbs as 1 and return
+;; 3. If the row and column exist, one of these things happen
+;;    3a. :curr-orbs+1 is < :max-orbs
+;;         3a1. :owner and player don't match -> Do nothing
+;;         3a2. :owner and player match -> Increment current orbs and send back a new map
+;;    3b. :curr-orbs+1 is >= :max-orbs
+;;         3b1. Chain react, all orbs in the path now have :owner = player
 
 (defn get-new-board
-  ([old-board row col player board-size] 
-    (get-new-board old-board row col player board-size false))
+  ([old-board row col player board-size]
+     (get-new-board old-board row col player board-size false))
   ([old-board row col player board-size is-react]
-   (cond
-    (= (is-valid-rc row col board-size) true)
+     (cond
+      (= (is-valid-rc row col board-size) true)
       (cond
-        (= (old-board row) nil) 
-          (assoc old-board row {col {:owner player :max-orbs (get-max-orbs row col board-size) :curr-orbs 1}})
-        (= ((old-board row) col) nil)
-          (assoc old-board row (assoc (old-board row) col {:owner player :max-orbs (get-max-orbs row col board-size) :curr-orbs 1}))
-        (< (inc (:curr-orbs ((old-board row) col))) (:max-orbs ((old-board row) col)))
-          (cond
-            (or (= player (:owner ((old-board row) col))) (= is-react true))
-              (assoc old-board row (assoc (old-board row) col 
-                  {:owner player 
-                   :max-orbs (:max-orbs ((old-board row) col)) 
-                   :curr-orbs  (inc (:curr-orbs ((old-board row) col)))
-                   }))
-            :else
-              old-board
-          )
-        (>= (inc (:curr-orbs ((old-board row) col))) (:max-orbs ((old-board row) col)))
-          (chain-react (assoc old-board row (dissoc (get old-board row) col)) row col player board-size)
-      ))))
+       (= (old-board row) nil)
+       (assoc old-board row {col {:owner player :max-orbs (get-max-orbs row col board-size) :curr-orbs 1}})
+       (= ((old-board row) col) nil)
+       (assoc old-board row (assoc (old-board row) col {:owner player :max-orbs (get-max-orbs row col board-size) :curr-orbs 1}))
+       (< (inc (:curr-orbs ((old-board row) col))) (:max-orbs ((old-board row) col)))
+       (cond
+        (or (= player (:owner ((old-board row) col))) (= is-react true))
+        (assoc old-board row (assoc (old-board row) col
+                                    {:owner player
+                                     :max-orbs (:max-orbs ((old-board row) col))
+                                     :curr-orbs  (inc (:curr-orbs ((old-board row) col)))
+                                     }))
+        :else
+        old-board
+        )
+       (>= (inc (:curr-orbs ((old-board row) col))) (:max-orbs ((old-board row) col)))
+       (chain-react (assoc old-board row (dissoc (get old-board row) col)) row col player board-size)
+       ))))
 
 
-;This method finds which player the next turn belongs to
+;;This method finds which player the next turn belongs to
 (defn get-next-player
-    [old-player player-size]
-    (if (= player-size (inc old-player))
-              0   
-              (inc old-player)))  
+  [old-player player-size]
+  (if (= player-size (inc old-player))
+    0
+    (inc old-player)))
 
-;Helper Methods to find the Winner, if any
+;;Helper Methods to find the Winner, if any
 
-;Given the board map this functions extracts only the cells for non nil row col values
-(defn get-all-cells 
-  [board board-size] 
-  (filter 
-    #(not= % nil) 
-    (for 
-      [x (range board-size) y (range board-size)] 
-      (get-in board [x y])))) 
+;;Given the board map this functions extracts only the cells for non nil row col values
+(defn get-all-cells
+  [board board-size]
+  (filter
+   #(not= % nil)
+   (for
+       [x (range board-size) y (range board-size)]
+     (get-in board [x y]))))
 
-;This method returns count of all cells that have :owner nil
+;;This method returns count of all cells that have :owner nil
 (defn count-of-nil-cells
-  [board board-size] 
+  [board board-size]
   (count (filter #(= (:owner %) nil) (get-all-cells board board-size))))
 
-;This method returns the count of cells a player owns, given board and player
-(defn count-of-player-cells 
-  [board player board-size] 
+;;This method returns the count of cells a player owns, given board and player
+(defn count-of-player-cells
+  [board player board-size]
   (count (filter #(= (:owner %) player) (get-all-cells board board-size))))
 
-;This method excludes the :owner nil cells and returns the count of valid(owned by a player) cells on the board
-(defn count-of-valid-cells 
-  [board board-size] 
+;;This method excludes the :owner nil cells and returns the count of valid(owned by a player) cells on the board
+(defn count-of-valid-cells
+  [board board-size]
   (- (count (get-all-cells board board-size)) (count-of-nil-cells board board-size)))
 
-;This method returns a list containing a single winner if any or empty list
+;;This method returns a list containing a single winner if any or empty list
 (defn winner
   [board board-size player-size]
   (filter
-    #(and (> (count-of-player-cells board % board-size) ) 
-          (= (count-of-player-cells board % board-size) (count-of-valid-cells board board-size)))
-    (range player-size)))
+   #(and (> (count-of-player-cells board % board-size) )
+         (= (count-of-player-cells board % board-size) (count-of-valid-cells board board-size)))
+   (range player-size)))
 
-;(defn -main [& args]
-;  (try (println(get-new-board {1 {2 {:owner 1, :max-orbs 4, :curr-orbs 3}}} 1 2 1 8 2 ))
-;       (catch Exception e (println e))))
-
+;; (defn -main [& args]
+;;  (try (println(get-new-board {1 {2 {:owner 1, :max-orbs 4, :curr-orbs 3}}} 1 2 1 8 2 ))
+;;       (catch Exception e (println e))))
--- src/chainreact_clj/interface.clj
+++ src/chainreact_clj/interface.clj
@@ -3,74 +3,74 @@
         [clojure.pprint]
         [clojure.string :only (split)]))
 
-;Splits command line input in style "row col" and returns '(row col)
-(defn split-input 
+;;Splits command line input in style "row col" and returns '(row col)
+(defn split-input
   [input]
   (map #(read-string %) (split input #" ")))
 
-;Returns printable value for a given cell
+;;Returns printable value for a given cell
 (defn get-print-value
   [cell]
   (let [owner (:owner cell)
         curr (:curr-orbs cell)
         maxm (:max-orbs cell)]
-    (str 
-      (apply str (repeat curr owner)) 
-      (apply str (repeat (- maxm curr) "_"))
-      (apply str (repeat (- 4 maxm) " ")))))
+    (str
+     (apply str (repeat curr owner))
+     (apply str (repeat (- maxm curr) "_"))
+     (apply str (repeat (- 4 maxm) " ")))))
 
-;Returns a list of printable values for all cells of a given row
+;;Returns a list of printable values for all cells of a given row
 (defn get-row-values
   [row board board-size]
-  (for [col (range board-size)] 
+  (for [col (range board-size)]
     (let [cell (get-in board [row col])
-          length (get-max-orbs row col board-size)] 
-      (if (= cell nil) 
+          length (get-max-orbs row col board-size)]
+      (if (= cell nil)
         (str (apply str (repeat length "+" )) (apply str (repeat (- 4 length) " ")))
         (get-print-value cell)))))
 
-;Draws the board to the console using pprint
-(defn draw-board 
+;;Draws the board to the console using pprint
+(defn draw-board
   [board board-size]
   (pprint (for [row (range board-size)]
             (get-row-values row board board-size)))
   )
 
-;The main method that enters into read loop, reads the move position 
-; from the player and calls draw-board to draw a new board to the console
+;;The main method that enters into read loop, reads the move position
+;; from the player and calls draw-board to draw a new board to the console
 (defn play
   [board-size player-size]
-    (loop [input (read-line)
-               board {}
-               player 0
-               move-count 0]
-          (when-not (= ":q" input) 
-            (let [new-board (get-new-board 
-                              board 
-                              (first (split-input input))
-                              (second (split-input input))
-                              player
-                              board-size)
-                  new-player (if (= new-board board) 
-                               player
-                               (get-next-player player player-size))
-                  new-move-count (if (= new-board board)
-                                   move-count
-                                   (inc move-count))]
-              (draw-board new-board board-size) 
-              (println (str "Player " new-player "'s turn"))
-              (if (and 
-                    (= (count (winner new-board board-size player-size)) 1)
-                    (> new-move-count 2))
-                      (println (str "Player " (first (winner new-board board-size player-size)) " WON"))
-                  (recur (read-line) new-board new-player new-move-count))))))
+  (loop [input (read-line)
+         board {}
+         player 0
+         move-count 0]
+    (when-not (= ":q" input)
+      (let [new-board (get-new-board
+                       board
+                       (first (split-input input))
+                       (second (split-input input))
+                       player
+                       board-size)
+            new-player (if (= new-board board)
+                         player
+                         (get-next-player player player-size))
+            new-move-count (if (= new-board board)
+                             move-count
+                             (inc move-count))]
+        (draw-board new-board board-size)
+        (println (str "Player " new-player "'s turn"))
+        (if (and
+             (= (count (winner new-board board-size player-size)) 1)
+             (> new-move-count 2))
+          (println (str "Player " (first (winner new-board board-size player-size)) " WON"))
+          (recur (read-line) new-board new-player new-move-count))))))
 
 
-;Reads command line arguments for board-size and player-size and starts the Play
+;;Reads command line arguments for board-size and player-size and starts the Play
 (defn -main [& args]
   (let [board-size (read-string (first args))
         player-size (read-string (second args))]
-        (draw-board {} board-size) 
-        (println (str "Player 0's turn"))
-        (play board-size player-size)
-        ))
+    (draw-board {} board-size)
+    (println (str "Player 0's turn"))
+    (play board-size player-size)
+    ))
